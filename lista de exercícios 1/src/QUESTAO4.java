177f8c30f57dedd340d7b5cc6a53c402c30d7278
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2023-08-17 08:42:27 -0300

    commit questao 4 finalizada

--- lista de exercícios 1/src/QUESTAO4.java
+++ lista de exercícios 1/src/QUESTAO4.java
@@ -2,35 +2,32 @@ import java.util.*;
 
 public class QUESTAO4 {
 
-    public static boolean testaFim(String palavra) {
-        boolean teste = false;
-
-        if (palavra.equals("FIM")) {
-            teste = true;
-        }
-
-        return teste;
+    public static boolean testaFim(String palavra) { //função booleana que testa se a palavra FIM foi digitada;
+        return palavra.equals("FIM");                //caso palavra.equals for verdadeiro, retornará true.
     }
 
-    public static void inverter(String palavra) {
-        String invertida="";
+    public static void inverter(String palavra) {    //método (sem retorno) que imprimirá o resultado da inversão da palavra
+        String invertida = "";
         int tamanho = palavra.length(); 
 
-        for(int i=tamanho-1; i>=0; i--) {
-            invertida += palavra.charAt(i); 
+        for(int i = tamanho - 1; i >= 0; i--) {
+            invertida += palavra.charAt(i);          
         }
 
-        System.out.print(invertida); 
+        System.out.println(invertida); 
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String palavra = input.next();
 
-        while (testaFim(palavra)) {
+        while (!testaFim(palavra)) {               //estrutura de repetição para continuar a leitura dos dados lidos pelo teclado enquanto a condição for FALSE
             inverter(palavra);
+            palavra = input.next();
         }
 
         input.close();
     }
 }
+
+
