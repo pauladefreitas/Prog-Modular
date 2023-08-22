915fd63ea22bc72dd7371786a1f95e3d9e018e8e
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2023-08-22 11:54:11 -0300

    q05 nao finalizada

---
+++ lista de exercícios 1/src/QUESTAO5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class QUESTAO5 {
+
+    int calculaEstudo(String tempoEstudo) {
+        
+    }
+
+    String formataHora() {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+        int tempoEstudo = input.next(); 
+
+        calculaEstudo(tempoEstudo); 
+    }
+}
