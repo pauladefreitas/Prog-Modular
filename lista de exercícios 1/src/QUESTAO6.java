420b86edd67d806fbdf519d5517a8b5b55d33ac4
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2023-08-23 10:57:15 -0300

    commit q06 nao finalizada

---
+++ lista de exercícios 1/src/QUESTAO6.java
@@ -0,0 +1,21 @@
+import java.util.*; 
+
+public class QUESTAO6 {
+
+    int premio1, premio2; 
+
+    void leituraValores() {
+        Scanner input = new Scanner(System.in);
+        this.premio1 = input.nextInt(); 
+        this.premio2 = input.nextInt(); 
+
+        
+    }
+
+    public static void main(String[] args) {
+        
+        
+
+        
+    }
+}
