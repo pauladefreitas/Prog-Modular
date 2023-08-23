4df85bef849fb33d239c19b01a110b3b814cedab
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2023-08-23 11:19:18 -0300

    q06 finalizada

--- lista de exercícios 1/src/QUESTAO6.java
+++ lista de exercícios 1/src/QUESTAO6.java
@@ -2,20 +2,31 @@ import java.util.*;
 
 public class QUESTAO6 {
 
-    int premio1, premio2; 
+    int premio1, premio2;
 
     void leituraValores() {
         Scanner input = new Scanner(System.in);
         this.premio1 = input.nextInt(); 
         this.premio2 = input.nextInt(); 
 
-        
+        input.close();
+    }
+
+    int numPremio() {
+        int valorPremio; 
+        premio1%=1000;
+        premio2/=100; 
+
+        valorPremio = (premio2*1000)+premio1;
+
+        return valorPremio; 
     }
 
     public static void main(String[] args) {
         
-        
-
+        QUESTAO6 teste = new QUESTAO6();
+        teste.leituraValores();
+        System.out.println(teste.numPremio());
         
     }
 }
