fc0e6417b85675afa8212120647956e8821be7f5
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2023-08-28 16:57:31 -0300

    q05 finalizada

--- lista de exercícios 1/src/QUESTAO5.java
+++ lista de exercícios 1/src/QUESTAO5.java
@@ -2,17 +2,26 @@ import java.util.*;
 
 public class QUESTAO5 {
 
-    int calculaEstudo(String tempoEstudo) {
-        
-    }
-
-    String formataHora() {
-
+    static void calculaEstudo(String tempoEstudo) {
+        String[] partes = tempoEstudo.split(" "); 
+        int materias = Integer.parseInt(partes[0]);
+    
+        String[] divHM = partes[1].split(":"); 
+        int hora = Integer.parseInt(divHM[0]); 
+        int minuto = Integer.parseInt(divHM[1]);
+    
+        int totalMinutos = (hora * 60) + minuto;
+        int totalStudyTime = totalMinutos / materias;
+    
+        int totalBreaks = materias - 1;
+        int breakTimePerBreak = (totalMinutos - totalStudyTime) / totalBreaks;
+    
+        System.out.print("O tempo de estudo é " + totalStudyTime + " com " + breakTimePerBreak + " de descanso.");
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); 
-        int tempoEstudo = input.next(); 
+        String tempoEstudo = input.nextLine(); 
 
         calculaEstudo(tempoEstudo); 
     }
