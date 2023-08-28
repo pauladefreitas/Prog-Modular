543712282bbba8cd4b039a8fad2b753227a278a2
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2023-08-28 17:24:13 -0300

    q08 finalizada

--- lista de exercícios 1/src/QUESTAO8.java
+++ lista de exercícios 1/src/QUESTAO8.java
@@ -4,59 +4,57 @@ public class QUESTAO8 {
     
     byte dia, mes;
     int ano;
+    static final int[] DIASDOMES = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     
-    /** Recebe a data digitada pelo usuário e armazena em uma variável no main.
-     * @return String data.
-     */
     String receberData() {
-
         Scanner input = new Scanner(System.in); 
         String data = input.next();
 
         if (data.length() != 10) {
-            System.out.print("Data diferente de 10 caracteres.");
+            return "Data diferente de 10 caracteres.";
         }
 
-        ajustar(data);
-
-        return data;
+        return ajustar(data);
     }
 
-    
-    /** Ajusta a data de maneira que os campos dia, mês e ano sejam devidamente preenchidos e, também, chama o método de validação
-     * para ajuste do preenchimento destes campos.
-     * Caso um deles não esteja conforme, reseta os valores para 01/01/1900.
-     * @param data
-     */
-    void ajustar(String data) {
+    String ajustar(String data) {
         String[] partes = data.split("/"); 
-        String resposta;
 
         this.dia = Byte.parseByte(partes[0]); 
         this.mes = Byte.parseByte(partes[1]); 
         this.ano = Integer.parseInt(partes[2]);
 
         int maxDia;
-        
-        if(ano<1900)
-            resposta =  "Ano inválido.";
-        else{
-           if (mes < 1 || mes > 12)                           
-                resposta =  "Mês inválido.";
-           else { 
-                   maxDia = DIASDOMES[mes];
-                   if(anoBissexto(ano)&&mes==2)     
-                        maxDia++;
-                   
-                   if (dia > maxDia)                
-                        resposta =  "Dia inválido: máximo de "+maxDia+" para o mês "+mes+".";
-                }
-        }                       
-        return resposta;
 
+        if (ano < 1900)
+            return "Ano inválido.";
+        else {
+            if (mes < 1 || mes > 12)
+                return "Mês inválido.";
+            else {
+                maxDia = DIASDOMES[mes];
+                if (anoBissexto(ano) && mes == 2)
+                    maxDia++;
 
+                if (dia > maxDia)
+                    return "Dia inválido: máximo de " + maxDia + " para o mês " + mes + ".";
+            }
+        }
+
+        return "Data válida.";
     }
 
+    boolean anoBissexto(int ano) {
+        return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+    }
 
-    
+    public static void main(String[] args) {
+        QUESTAO8 validarData = new QUESTAO8();
+
+        System.out.println("Digite uma data no formato dd/mm/aaaa:");
+        String data = validarData.receberData();
+
+        String resposta = validarData.ajustar(data);
+        System.out.println(resposta);
+    }
 }
